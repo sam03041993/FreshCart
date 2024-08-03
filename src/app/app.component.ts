9b9a8533fc007e16997f69fd57a3a8452ce2ffe9
sam03041993 <60146725+sam03041993@users.noreply.github.com>
2024-08-03 19:35:12 +0530

    import register component in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports:[NavBarComponent,RouterOutlet,FooterComponent],
+  imports:[NavBarComponent,RouterOutlet,FooterComponent,RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
